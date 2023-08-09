025b624b4e6f976afc7887601b25186e7168d53c
Merdan D <merdan.durdiyev@gmail.com>
2023-08-09 14:36:04 +0500

    Adds associations to Product and Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
 
   # VALIDATIONS
   validates :name, presence: true, length: { minimum: 3 }
+
+  # ASSOCIATIONS
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+
+  # ASSOCIATIONS
+  belongs_to :category
 end
