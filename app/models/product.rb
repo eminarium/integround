57ae71f14590caf51eedf8a3a79c5b08c6d99ab8
Merdan D <merdan.durdiyev@gmail.com>
2023-08-09 14:41:31 +0500

    Adds basic validations to Product and Category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
 
+  # VALIDATIONS
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :category_id, presence: true
+
   # ASSOCIATIONS
   belongs_to :category
 end
